e64ab536c6ab6d8f0073c6e258987a5d4eec13c4
Bruce M. Axtens <bugmagnet@outlook.com>
2019-08-14 21:31:37 +0800

    cleanup

--- Collema/Program.cs
+++ Collema/Program.cs
@@ -1,15 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Collema
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            TaskScheduler.TaskScheduler ts;
+            ts = new TaskScheduler.TaskScheduler();
+            ts.Connect();
+            if (ts.Connected)
+            {
+                var ans = ts.GetFolder(@"\");
+                var anss = ans.GetFolders(0);
+                int c = anss.Count;
+
+                System.Diagnostics.Debugger.Launch();
+
+            }
+
+        }
+        /// <summary>
+        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
+        /// Defaults to ASCII when detection of the text file's endianness fails.
+        /// </summary>
+        /// <param name="filename">The text file to analyze.</param>
+        /// <returns>The detected encoding.</returns>
+        public static Encoding GetEncoding(string filename)
+        {
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
+            return Encoding.Default;
         }
     }
 }
